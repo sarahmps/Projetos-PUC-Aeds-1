b0741ffab522c68bdfda7dc1b3ad3b613db2443e
Sarah <sarahmpedrosa29@gmail.com>
2026-09-19 18:44:58 -0300

    Add files via upload

---
+++ VERIFICAR SE UM NATURAL É POTÊNCIA DE 2 ITERATIVO/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <stdbool.h>
+
+void apresentacao()
+{
+    printf("\n\tVerificar Se Um Número Natural É Uma Potência De 2\n");
+}
+
+int lerNum()
+{
+    int num;
+    do{
+        printf("\nDigite um número natural qualquer: ");
+        scanf("%i", &num);
+        if(num < 0){
+            printf("\n\aValor inválido! Por favor digite um número maior ou igual a 0.\n");
+        }
+    }while(num < 0);
+    return num;
+}
+
+bool potencia(int num)
+{
+    if(num <= 0){
+        return false;
+    }
+    while(num % 2 == 0){
+        num = num / 2;
+    }
+    return num == 1;
+}
+
+void resultado(int num, bool pot)
+{
+    if(pot == true){
+        printf("\nO número %i é uma potência de 2.\n", num);
+    }else{
+        printf("\nO número %i NÃO é uma potência de 2.\n", num);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    apresentacao();
+    int num = lerNum();
+    int pot = potencia(num);
+    resultado(num, pot);
+    return 0;
+}
